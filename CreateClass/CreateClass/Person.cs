a89c662e8dd2d0bbef266b3eab6415ce041294d4
cickib <bellakocsis06@gmail.com>
2017-06-05 18:55:41 +0200

    Person class done

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace CreateClass
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Genders gender;
+
+        public Person() { }
+        public Person(string name, DateTime birthDate)
+        {
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("name: {0}, birth date: {1}", this.Name, this.BirthDate);
+        }
+
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "bella";
+            DateTime date = new DateTime(1990, 12, 6);
+            person.BirthDate = date;
+            Console.WriteLine(person);
+            Console.ReadKey();
+        }
+    }
+
+    public enum Genders : int
+    {
+        Male, Female
+    }
+}
