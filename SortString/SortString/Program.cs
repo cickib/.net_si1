a1000d1d2a94d01ad3617f09e08f9a24244fc6f7
cickib <bellakocsis06@gmail.com>
2017-06-05 19:20:54 +0200

    string sorting exercise done

---
+++ SortString/SortString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string randomString = "The word 'word' supposed to come last when sorted. Such a random string.";
+            string[] stringArr = randomString.Split(' ');
+            Array.Sort(stringArr);
+            string result = string.Join(" ", stringArr);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
