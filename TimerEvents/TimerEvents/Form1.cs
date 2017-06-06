785c120dfced8dad85e62be20f1d7f12be1ad8f8
cickib <bellakocsis06@gmail.com>
2017-06-06 19:04:27 +0200

    progressbar implemented

---
+++ TimerEvents/TimerEvents/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TimerEvents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(1000);
+        }
+    }
+}
